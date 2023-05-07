01c9cb12da2e91406bf0e5b3ce0e530a82f8a005
EvansRobbie <evansitoden94@gmail.com>
2023-05-07 23:35:43 +0300

    date fixed

--- frontend/src/components/BookDates.tsx
+++ frontend/src/components/BookDates.tsx
@@ -7,6 +7,36 @@ interface placeProps{
 }
 
 const BookDates = ({checkin, checkout, ifExists}:placeProps) => {
+    const formatDate = (date: string | undefined): string => {
+        if (!date) {
+          return '';
+        }
+    
+        const parsedDate = new Date(date);
+    
+        if (isNaN(parsedDate.getTime())) {
+          return '';
+        }
+    
+        return format(parsedDate, 'yyyy-MM-dd');
+      };
+    
+      const getNightCount = (): string => {
+        if (!checkin || !checkout) {
+          return '';
+        }
+    
+        const startDate = new Date(checkin);
+        const endDate = new Date(checkout);
+    
+        if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+          return '';
+        }
+    
+        const nightCount = differenceInCalendarDays(endDate, startDate);
+    
+        return `${nightCount} ${nightCount === 1 ? 'night' : 'nights'}`;
+      };
   return (
     <div className={`${ifExists ? 'flex flex-col md:items-center gap-y-2' : ''} `}>
     <div className={`${ifExists ? 'flex-col md:flex-row gap-x-4 md:items-center' : 'flex-col '} flex  `}> 
@@ -16,7 +46,7 @@ const BookDates = ({checkin, checkout, ifExists}:placeProps) => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                 </svg>
-                <span className="text-sm"> {format(new Date(checkin), 'yyyy-MM-dd')} </span>
+                <span className="text-sm"> {formatDate(checkin)} </span>
             </div>
 
         </div>
@@ -29,7 +59,7 @@ const BookDates = ({checkin, checkout, ifExists}:placeProps) => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                 </svg>
 
-                <span className="text-sm">{format(new Date(checkout), 'yyyy-MM-dd')}</span>
+                <span className="text-sm">{formatDate(checkout)}</span>
             </div>
         </div>
 </div>
@@ -39,7 +69,7 @@ const BookDates = ({checkin, checkout, ifExists}:placeProps) => {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="rotate-180 w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
         </svg>
-            {differenceInCalendarDays(new Date(checkout), new Date(checkin))} {differenceInCalendarDays(new Date(checkout), new Date(checkin)) === 1 ? 'night': 'nights'} <br />
+           {getNightCount()} <br />
     </div>
 </div>
 
